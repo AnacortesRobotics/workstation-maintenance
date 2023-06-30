ab3c92913c26c5cc7662df96db0cce993cf76e37
CoachJenkins <22876098+anacortesftcrobotics@users.noreply.github.com>
2023-06-30 12:41:54 -0700

    Coach submitted first version of workstation maintenance scripts.

---
+++ ComputerReport.ps1
@@ -0,0 +1,86 @@
+﻿# Anacortes Robotics
+# Report on workstations (FTC + FLL Challenge)
+# Author: Coach Jenkins
+# Created: 6/25/2023
+# Version: 1.0
+
+# Generic variables
+$delimiter = '|'
+$currentDate = Get-Date -Format 'yyyy-MM-dd'
+$currentPath = $PWD.Path + '\reports'
+
+Write-Host ""
+Write-Host ""
+
+# Get Data - Device info, installed apps info
+$SysInfo = Get-ComputerInfo
+$OSInfo = Get-CimInstance Win32_OperatingSystem
+$RamInfo = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
+$DiskInfo = (Get-WmiObject -Class Win32_LogicalDisk | ? {$_. DriveType -eq 3})
+$Properties = [ordered]@{ 
+    Device           = $SysInfo.CsDNSHostName
+    Model            = $SysInfo.CsModel
+    Processor        = $SysInfo.CsProcessors.Name
+    InstalledRam     = $RamInfo.ToString('#.# GB')
+    SystemType       = $SysInfo.CsSystemType
+    Num_Processors   = $SysInfo.CsNumberOfLogicalProcessors   
+    Edition          = $SysInfo.OSName                      
+    WindowsVersion   = $SysInfo.OsDisplayVersion 
+    InstallDate      = $OSInfo.InstallDate
+    Build            = $SysInfo.OSBuildNumber
+    HotFixes         = $SysInfo.OsHotFixes
+    BiosStatus       = $SysInfo.BiosStatus 
+    TotalDiskSpace   = ($DiskInfo.Size /1GB).ToString('#.# GB')
+    FreeDiskSpace    = ($DiskInfo.FreeSpace /1GB).ToString('#.# GB')
+}
+
+$DeviceInfo = [pscustomobject]$Properties
+
+$DeviceInfo
+
+#Get List of Installed Apps
+
+$excludedPublishers = @('NVIDIA*','Conexant*','Microsoft*')
+$filteredInstalledApps = Get-ItemProperty hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Where-Object { 
+        $pub = $_.Publisher
+        $exclude = $false
+        foreach ($excludedPublisher in $excludedPublishers) {
+            if ($pub -like $excludedPublisher) {
+                $exclude = $true
+                break
+            }
+        }
+        -not $exclude
+    }
+
+$filteredInstalledApps | Sort-Object -Property Publisher | Select-Object Publisher,DisplayName,DisplayVersion,InstallDate | Format-Table
+
+$csvDevicePath = "$($currentPath)\$($DeviceInfo.Device)-Report-Device-$($currentDate).csv"
+
+# clear the output file first
+if (Test-Path -Path $csvDevicePath) {
+    Clear-Content -Path $csvDevicePath
+}
+# get the desired field, convert to CSV string, 
+# remove lines with only delimiter characters (empty...), remove double quotes, output the file
+$DeviceInfo |
+    ConvertTo-Csv -Delimiter $delimiter -NoTypeInformation | %{$_ -replace '"',''} |
+    Where-Object { $_ -notmatch '^(.)\1*$'} |
+    Out-File -FilePath $csvDevicePath -Append -Encoding utf8
+
+
+
+$csvAppsPath = "$($currentPath)\$($DeviceInfo.Device)-Report-InstalledApps-$($currentDate).csv"
+
+# clear the output file first
+if (Test-Path -Path $csvAppsPath) {
+    Clear-Content -Path $csvAppsPath
+}
+# get the desired field, convert to CSV string, 
+# remove lines with only delimiter characters (empty...), remove double quotes, output the file
+$filteredInstalledApps | Select-Object Publisher,DisplayName,DisplayVersion,InstallDate | 
+    Sort-Object -Property Publisher |
+    ConvertTo-Csv -Delimiter $delimiter -NoTypeInformation | %{$_ -replace '"',''} |
+    Where-Object { $_ -notmatch '^(.)\1*$'} |
+    Out-File -FilePath $csvAppsPath -Append -Encoding utf8
---
+++ choco_install.ps1
@@ -0,0 +1,50 @@
+﻿# Anacortes Robotics
+# Chocolately install script for workstations (FTC + FLL Challenge)
+# Author: Coach Jenkins
+# Created: 6/21/2023
+# Version: 1.0
+
+# Got ideas from:
+# https://gist.github.com/apfelchips/792f7708d0adff7785004e9855794bc0
+# Youtube video on hook scripts...
+
+# Set the execution policy for this run only
+Set-ExecutionPolicy RemoteSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Check Permissions
+if ( -Not( (New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) ){
+    Write-Error -Message "Script needs Administrator permissions"
+    exit 1
+}
+
+if (-Not (Get-Command "choco" -errorAction SilentlyContinue)) {
+   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+   iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+
+# Generally setup code using Admin privileges
+choco feature enable -n=allowGlobalConfirmation
+
+$configfile = "packages.config"
+# Test that packages.config exists with this file
+if (Test-Path -Path $configfile) {
+    # Run the installs from the config file
+    choco install $configfile -s $(pwd) --verbose --debug
+} else {
+    Write-Error -Message "Config file is missing: $configfile"
+}
+
+# What's left as of 6/25/2023
+
+### Does not work - need to develop new community package
+#    <package id="revrobotics-hardwareclient" source="https://community.chocolatey.org/api/v2/"
+#        version="1.5.3"
+#        allowMultipleVersions="false" ignoreDependencies="false"
+#    />
+
+# freecad is not installed into InstallDir parameter, need to fix in package.config
+
+# Need to add IntelliJ and Amazon Corretto 11, then make sure env vars are all correct
+# need to make community package for Ev3 Classroom or newest Spike Classroom?
