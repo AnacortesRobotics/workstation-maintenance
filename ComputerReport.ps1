aa09b439dd44129cdaf1f163d106ec521194e27b
CoachJenkins <22876098+anacortesftcrobotics@users.noreply.github.com>
2023-07-01 10:45:52 -0700

    Add output of users on machine, break report code up into functions, start adding function documentation.

--- ComputerReport.ps1
+++ ComputerReport.ps1
@@ -1,45 +1,102 @@
 ﻿# Anacortes Robotics
 # Report on workstations (FTC + FLL Challenge)
 # Author: Coach Jenkins
-# Created: 6/25/2023
-# Version: 1.0
+# Created: 06-25-2023
+# Version: 1.1
+# Update 1.1/07-01-2023:
+#   * Add output of all users on workstation
+#   * Cleanup ComputerReport.ps1 file to use functions, read more easily
+#   * Add documentation to functions
 
 # Call from commandline:
 # .\ComputerReport.ps1
+
+function Get-DeviceInfo {
+    <#
+    .SYNOPSIS
+        Get information matching "About Device" into a custom object.
+
+    .DESCRIPTION
+        Retrieves data about the device and the windows OS for saving to file.
+
+    .INPUTS
+        None.
+
+    .OUTPUTS
+        PSCustomObject containing fields matching those of About device.
+
+    .EXAMPLE
+        $customObj = Get-DeviceInfo
+    #>
+    # Get Data - Device info, installed apps info
+    $SysInfo = Get-ComputerInfo
+    $OSInfo = Get-CimInstance Win32_OperatingSystem
+    $RamInfo = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
+    $DiskInfo = (Get-WmiObject -Class Win32_LogicalDisk | ? {$_. DriveType -eq 3})
+    $Properties = [ordered]@{
+        Device           = $SysInfo.CsDNSHostName
+        Model            = $SysInfo.CsModel
+        Processor        = $SysInfo.CsProcessors.Name
+        InstalledRam     = $RamInfo.ToString('#.# GB')
+        SystemType       = $SysInfo.CsSystemType
+        Num_Processors   = $SysInfo.CsNumberOfLogicalProcessors
+        Edition          = $SysInfo.OSName
+        WindowsVersion   = $SysInfo.OsDisplayVersion
+        InstallDate      = $OSInfo.InstallDate
+        Build            = $SysInfo.OSBuildNumber
+        HotFixes         = $SysInfo.OsHotFixes
+        BiosStatus       = $SysInfo.BiosStatus
+        TotalDiskSpace   = ($DiskInfo.Size /1GB).ToString('#.# GB')
+        FreeDiskSpace    = ($DiskInfo.FreeSpace /1GB).ToString('#.# GB')
+    }
+
+    $DeviceInfo = [PsCustomObject]$Properties
+
+    return $DeviceInfo
+}
+
+function Write-DataCsv {
+    param(
+        [Parameter()]
+        [String] $OutputFileNamePrefix,
+        [String] $OutputFilePostfix,
+        [String] $Delimiter,
+        [PsCustomObject] $OutputInfo
+    )
+    $OutputFile = "$OutputFileNamePrefix-$OutputFilePostfix.csv"
+
+    # clear the output file first
+    if (Test-Path -Path $OutputFile) {
+        Clear-Content -Path $OutputFile
+        Write-Host "in clear content"
+    }
+
+    # get the desired field, convert to CSV string,
+    # remove lines with only delimiter characters (empty...), remove double quotes, output the file
+    $OutputInfo |
+        ConvertTo-Csv -Delimiter $Delimiter -NoTypeInformation | %{$_ -replace '"',''} |
+        Where-Object { $_ -notmatch '^(.)\1*$'} |
+        Out-File -FilePath $OutputFile -Append -Encoding utf8
+}
+
+## Main
 
 # Generic variables
 $delimiter = '|'
 $currentDate = Get-Date -Format 'yyyy-MM-dd'
 $currentPath = $PWD.Path + '\reports'
 
-Write-Host ""
-Write-Host ""
-
-# Get Data - Device info, installed apps info
-$SysInfo = Get-ComputerInfo
-$OSInfo = Get-CimInstance Win32_OperatingSystem
-$RamInfo = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
-$DiskInfo = (Get-WmiObject -Class Win32_LogicalDisk | ? {$_. DriveType -eq 3})
-$Properties = [ordered]@{ 
-    Device           = $SysInfo.CsDNSHostName
-    Model            = $SysInfo.CsModel
-    Processor        = $SysInfo.CsProcessors.Name
-    InstalledRam     = $RamInfo.ToString('#.# GB')
-    SystemType       = $SysInfo.CsSystemType
-    Num_Processors   = $SysInfo.CsNumberOfLogicalProcessors   
-    Edition          = $SysInfo.OSName                      
-    WindowsVersion   = $SysInfo.OsDisplayVersion 
-    InstallDate      = $OSInfo.InstallDate
-    Build            = $SysInfo.OSBuildNumber
-    HotFixes         = $SysInfo.OsHotFixes
-    BiosStatus       = $SysInfo.BiosStatus 
-    TotalDiskSpace   = ($DiskInfo.Size /1GB).ToString('#.# GB')
-    FreeDiskSpace    = ($DiskInfo.FreeSpace /1GB).ToString('#.# GB')
+if (-Not [bool](Get-ChildItem -Path $currentPath -ErrorAction Ignore) )
+{
+    #PowerShell Create directory if not exists
+    New-Item $currentPath -ItemType Directory
 }
 
-$DeviceInfo = [pscustomobject]$Properties
+Write-Host ""
+Write-Host ""
 
-$DeviceInfo
+
+$DeviceInfo = Get-DeviceInfo
 
 #Get List of Installed Apps
 
@@ -66,32 +123,8 @@ $filteredInstalledApps = Get-ItemProperty hklm:\SOFTWARE\Microsoft\Windows\Curre
     }
 
 $filteredInstalledApps | Sort-Object -Property Publisher | Select-Object Publisher,DisplayName,DisplayVersion,InstallDate | Format-Table
+$UserData = $OutputInfo = Get-LocalUser | Select *
 
-$csvDevicePath = "$($currentPath)\$($DeviceInfo.Device)-Report-Device-$($currentDate).csv"
-
-# clear the output file first
-if (Test-Path -Path $csvDevicePath) {
-    Clear-Content -Path $csvDevicePath
-}
-# get the desired field, convert to CSV string, 
-# remove lines with only delimiter characters (empty...), remove double quotes, output the file
-$DeviceInfo |
-    ConvertTo-Csv -Delimiter $delimiter -NoTypeInformation | %{$_ -replace '"',''} |
-    Where-Object { $_ -notmatch '^(.)\1*$'} |
-    Out-File -FilePath $csvDevicePath -Append -Encoding utf8
-
-
-
-$csvAppsPath = "$($currentPath)\$($DeviceInfo.Device)-Report-InstalledApps-$($currentDate).csv"
-
-# clear the output file first
-if (Test-Path -Path $csvAppsPath) {
-    Clear-Content -Path $csvAppsPath
-}
-# get the desired field, convert to CSV string, 
-# remove lines with only delimiter characters (empty...), remove double quotes, output the file
-$filteredInstalledApps | Select-Object Publisher,DisplayName,DisplayVersion,InstallDate | 
-    Sort-Object -Property Publisher |
-    ConvertTo-Csv -Delimiter $delimiter -NoTypeInformation | %{$_ -replace '"',''} |
-    Where-Object { $_ -notmatch '^(.)\1*$'} |
-    Out-File -FilePath $csvAppsPath -Append -Encoding utf8
+Write-DataCsv "$($currentPath)\$($DeviceInfo.Device)-Report-$($currentDate)" "Device" $delimiter $DeviceInfo
+Write-DataCsv "$($currentPath)\$($DeviceInfo.Device)-Report-$($currentDate)" "InstalledApps" $delimiter $filteredInstalledApps
+Write-DataCsv "$($currentPath)\$($DeviceInfo.Device)-Report-$($currentDate)" "Users" $delimiter $UserData
