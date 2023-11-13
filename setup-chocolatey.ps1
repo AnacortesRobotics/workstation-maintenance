06f38c06785e031edd917f9b7ebb892fadbc59d1
CoachJenkins <22876098+anacortesftcrobotics@users.noreply.github.com>
2023-11-13 12:00:06 -0800

    Update setup-chocolatey.ps1 to add local source so packages.config can look locally first, before going to url for install.

--- setup-chocolatey.ps1
+++ setup-chocolatey.ps1
@@ -1,25 +1,15 @@
-#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 $Executable = "choco.exe"
+$MinimumVersion = [System.Version]"2.2.0"
+$elevated = ([Security.Principal.WindowsPrincipal] `
+ [Security.Principal.WindowsIdentity]::GetCurrent()
+).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-if (Get-Command $Executable -ErrorAction SilentlyContinue)
-{
-   $CurrentVersion = [System.Version](choco.exe --version)
-   Write-Host $CurrentVersion
-   $MinimumVersion = [System.Version]"2.2.0"
+# Get the most recent chocolatey msi installer file
+$InstallFile=(Get-ChildItem "..\*" -Filter choco*.msi -recurse  | sort LastWriteTime | select -last 1).FullName
+Write-Host $InstallFile
 
-   $RequiredVersion = [System.Version]$MinimumVersion
-
-   If ($CurrentVersion -lt $RequiredVersion)
-   {
-        Write-Host "$($Executable) version $($CurrentVersion) does not meet requirements. Upgrading ..."
-   }
-   else {
-        Write-Host "$($Executable) version $($CurrentVersion) meets requirements, greater than $($MinimumVersion). No action taken."
-   }
-}
-else {
-    Write-Host "Unable to find $($Executable) in your PATH. Installing ..."
-    $InstallFile = "..\chocolatey-2.2.2.0.msi"
+function runMSI {
+    $DataStamp = get-date -Format yyyyMMddTHHmmss
     $DataStamp = get-date -Format yyyyMMddTHHmmss
     $logFile = '{0}-{1}.log' -f $Executable,$DataStamp
     $MSIArguments = @(
@@ -29,15 +19,38 @@ else {
         "/L*v"
         $logFile
     )
-#         $MSIArguments = @(
-#         "/i"
-#         ('"{0}"' -f $Executable)
-#         "/qn"
-#         "/norestart"
-#         "/L*v"
-#         $logFile
-#     )
     $installed = Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
     Write-Host $installed
+}
+
+if (! $elevated) {
+    Write-Host "This script needs to be run in adminstorator account which this is not. Cancelling run."
+    Exit 0
+}
+if (Get-Command $Executable -ErrorAction SilentlyContinue)
+{
+    $CurrentVersion = [System.Version](Start-Process "$($Executable)" -ArgumentList "--version" -Wait -NoNewWindow)
+    Write-Host $CurrentVersion
+
+    $RequiredVersion = [System.Version]$MinimumVersion
+
+    If ($CurrentVersion -lt $RequiredVersion)
+    {
+        Write-Host "$($Executable) version $($CurrentVersion) does not meet requirements. Upgrading ..."
+        runMSI
+        Write-Host "restart machine to get it to recognize choco commands"
+    }
+    else {
+        Write-Host "$($Executable) version $($CurrentVersion) meets requirements, greater than $($MinimumVersion). No action taken."
+    }
+}
+else {
+    Write-Host "Unable to find $($Executable) in your PATH. Installing ..."
+#
+    runMSI
     Write-Host "restart machine to get it to recognize choco commands"
 }
+
+# Add local source pointing to assets directory of local nupkg files.
+choco source add --name="local" --source="assets\" --priority=1
+
